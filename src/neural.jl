19ac72984f9ca884b889bd61a99e414fd2e595e9
unknown <sanjay.dev.x@gmail.com>
2023-07-30 01:17:50 +0530

    cleaning up code, partially

--- src/neural.jl
+++ src/neural.jl
@@ -1,141 +1,186 @@
+#Packages 
 
 using Statistics
 using Flux
 using DataFrames
-using FileIO
 using MAT
+using Plots
 
+#Define the file paths
 
-
-# Define the file paths
 folder = "C:\\Users\\Dell\\Downloads\\Neural\\Relevant"
 foldertrain = joinpath(folder, "Train")
 foldertest = joinpath(folder, "Test")
 train_folder = joinpath(foldertrain,"data.mat")
 
+#Creating DataFrames
 
-#Creating Dataframes
+filetest_1 = DataFrame(V = [], I = [], Temperature = [], Capacity = [], Energy = [], SOC = [])
+filetest_2 = DataFrame(V = [], I = [], Temperature = [], Capacity = [], Energy = [], SOC = [])
+filetest_3 = DataFrame(V = [], I = [], Temperature = [], Capacity = [], Energy = [], SOC = [])
+filetest_4 = DataFrame(V = [], I = [], Temperature = [], Capacity = [], Energy = [], SOC = [])
+data_train = DataFrame(V = [], I = [], Temperature = [], Capacity = [], Energy = [], SOC = [])
+Error = DataFrame(File = [], Max = [],Mean = [],RMSE = [])
 
-filetest_1 = DataFrame(V = [], I = [], Capacity = [], Temperature = [], Something = [], SOC = [])
-filetest_2 = DataFrame(V = [], I = [], Capacity = [], Temperature = [], Something = [], SOC = [])
-filetest_3 = DataFrame(V = [], I = [], Capacity = [], Temperature = [], Something = [], SOC = [])
-filetest_4 = DataFrame(V = [], I = [], Capacity = [], Temperature = [], Something = [], SOC = [])
-data_train = DataFrame(V = [], I = [], Capacity = [], Temperature = [], Something = [], SOC = [])
+#Global Variables 
 
+model = 0
 
 #Opening and reading MAT files for testing
-for i in 1:4
-    file = matopen(foldertest*"\\data"*string(i)*".mat")
-    y_test = read(file, "Y")
-    x_test = read(file, "X")
-    y_test = DataFrame(y_test,:auto)
-    x_test = DataFrame(x_test,:auto)
-    y_test = dropmissing(y_test)
-    x_test = dropmissing(x_test)
-    if i == 1
-        global filetest_1 = vcat(x_test,y_test)
-    elseif i == 2
-        global filetest_2 = vcat(x_test,y_test)
-    elseif i == 3
-        global filetest_3 = vcat(x_test,y_test)
-    else
-        global filetest_3 = vcat(x_test,y_test)
+
+function openmatfiles_test!()
+    for i in 1:4
+        file = matopen(foldertest*"\\data"*string(i)*".mat")
+        y_test = read(file, "Y")
+        x_test = read(file, "X")
+        y_test = DataFrame(y_test,:auto)
+        x_test = DataFrame(x_test,:auto)
+        y_test = dropmissing(y_test)
+        x_test = dropmissing(x_test)
+        if i == 1
+            filetest_1 = vcat(x_test,y_test)
+        elseif i == 2
+            filetest_2 = vcat(x_test,y_test)
+        elseif i == 3
+            filetest_3 = vcat(x_test,y_test)
+        else
+            filetest_4 = vcat(x_test,y_test)
+        end
     end
 end
 
-# Opening and reading MAT files for training
-matfile = matopen(train_folder)
-y_train = read(matfile, "Y")
-x_train = read(matfile, "X")
-close(matfile)
+#Opening and reading MAT files for training
+function openmatfiles_train!()
+    matfile = matopen(train_folder)
+    y_train = read(matfile, "Y")
+    x_train = read(matfile, "X")
+    close(matfile)
 
+    df_y_train = DataFrame(y_train,:auto)
+    df_x_train = DataFrame(x_train,:auto)
+    df_y_train = dropmissing(df_y_train)
+    df_x_train = dropmissing(df_x_train)
 
-df_y_train = DataFrame(y_train,:auto)
-df_x_train = DataFrame(x_train,:auto)
-df_y_train = dropmissing(df_y_train)
-df_x_train = dropmissing(df_x_train)
+    data_train = vcat(df_x_train,df_y_train)
 
-#Concatenate 
-
-data_train = vcat(df_x_train,df_y_train)
-
-#Defining NN architecture 
-
-num_hidden_units = 55
-
-model = Chain(
-                Dense(3, num_hidden_units, tanh),
-                Dense(num_hidden_units, num_hidden_units, leakyrelu),
-                Dense(num_hidden_units, 1)
-            )
-
-#Defining Epochs, batch size and learning rate
-epochs = 50
-batch_size = 32
-learning_rate = 0.01
-
-#Defining optimizer and loss function
-
-opt = Flux.Optimise.ADAM(learning_rate)
-loss(x, y) = Flux.mse(model(x), y)
-
-#Converting data into arrays 
-x_train_array = Float32.(Matrix(data_train[1:3, :]))
-y_train_array = data_train[6,:]
-y_train_array = Vector(y_train_array)
-y_train_array = y_train_array[:,:]
-y_train_array =  transpose(y_train_array)
-
-#data = [(x_train_array,y_train_array)]
-
-dataset = Flux.Data.DataLoader((x_train_array,y_train_array), batchsize=batch_size, shuffle=true)
-
-#Training the model
-
-parameters = Flux.params(model)
-
-for i in 1:epochs
-    Flux.train!(loss, parameters, dataset, opt)
+    x_train = Float32.(Matrix(data_train[1:5, :]))
+    y_train = data_train[6,:]
+    y_train = Vector(y_train)
+    y_train = y_train[:,:]
+    y_train =  transpose(y_train)
 end
 
-#Testing
 
-testfile_4 = Float32.(data_train)
+#Defining Architecture and Parameters
 
-predictions_1 = []
+function define_parameters!(num_hidden_units,first_layer,last_layer,batch_size,initial_learningrate,decay1, decay2)
 
-for i in 1:size(testfile_4,2)
-    prediction = model(testfile_4[1:3,i])
-    push!(predictions_1,prediction)
-end 
+    model = Chain(
+                    Dense(first_layer, num_hidden_units, tanh),
+                    Dense(num_hidden_units, num_hidden_units, leakyrelu),
+                    Dense(num_hidden_units, last_layer)
+                )
 
-CSV_df = DataFrame(SOC_predicted = predictions_1)
+    opt = Flux.Optimise.ADAM(initial_learningrate,(decay1, decay2), eps(typeof(initial_learningrate)))
 
-using CSV
-CSV.write(joinpath(folder,"SOC_predicted.csv"), CSV_df)
+    batch_size = batch_size
+end
 
-#=
-Target_1 = testfile_4[6, :]
+#Loss function
+
+function loss()
+    loss(x, y) = Flux.mse(model(x), y)
+end
 
 
-error_test = []
-error_testsqr = []
-# Calculate the errors
-for i in 1:size(testfile_4,2)
-    temp = predictions_1[i]
-    error = temp[1] .-  Target_1[i]
-    push!(error_test,error)
-    errorsqr = error^2
-    push!(error_testsqr,errorsqr)
+#Loading data and training the model
+
+function train!(epochs,data_input,data_output,batch_size,opt)
+    local dataset = Flux.Data.DataLoader((data_input,data_output), batchsize = batch_size, shuffle=true)
+    local parameters = Flux.params(model)
+
+    for i in 1:epochs
+        Flux.train!(loss, parameters, dataset, opt)
+        println("Epoch "*string(i))
+    end
+end
+
+#Testing 
+
+function testing!(testing_file)
+    local Test_data = Float32.(testing_file)
+    local predictions_1 = []
+    local error_test = []
+    local error_testsqr = []
+    local Mean_error = []
+    local RMSE = []
+    local Max_error = []
+
+
+    Test_data = permutedims(Test_data)
+    Test_data = Test_data[:,6]*100
+
+    for i in 1:size(Test_data,2)
+        prediction = model(Test_data[1:5,i])
+        prediction =  prediction[1]
+        local scaled = prediction*100
+        push!(predictions_1,scaled)
+    end
+
+
+    # Calculate the errors
+    for i in 1:size(Test_data,2)
+        local temp = predictions_1[i]
+        local error = temp[1] .-  Test_data[i]
+        push!(error_test,error)
+        errorsqr = error^2
+        push!(error_testsqr,errorsqr)
+    end
+
+        # Compute RMSE, MAE, and MAX errors
+    rmse_test = sqrt(mean(error_testsqr))
+    mae_test = mean(abs.(error_test))
+    max_test = maximum(abs.(error_test))
+    push!(Max_error,max_test)
+    push!(Mean_error,mae_test)
+    push!(RMSE,rmse_test)
+
+    Error = DataFrame(File = string(testing_file), Max = Max_error,Mean = Mean_error,RMSE = RMSE)
 
 end
 
-# Compute RMSE, MAE, and MAX errors
-rmse_test = sqrt(mean(error_testsqr))
-mae_test = mean(abs.(error_test))
-max_test = maximum(abs.(error_test))
+function main()
 
-println(rmse_test)
-println(mae_test)
-println(max_test)
-=#
+    local x_train = []
+    local y_train = []
+    local batch_size = 0
+    local opt = 0
+
+    #Call function to open test and train files
+    openmatfiles_test!()
+    openmatfiles_train!()
+
+
+    #Call function to define parameters and hyperparameters
+    define_parameters!(55,5,1,32,0.01,0.9,0.95)
+
+    #Call function to define loss function
+    loss()
+
+    #Call function to train model on a number of epochs
+    train!(1,x_train,y_train,batch_size,opt)
+
+    #Call file for testing
+    testing!(filetest_1)
+    testing!(filetest_2)
+    testing!(filetest_3)
+    testing!(filetest_4)
+
+    #Display Error Stats
+
+    println(Error)
+
+end
+
+main()
+
